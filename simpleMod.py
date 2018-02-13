9fb4a6cd5a46959ffb64ffa1cedea73baf02fc3a
Simon aka @simille <simon@elisya.org>
2018-02-13 12:44:53 +0100

    simplify more simpleModel

--- simpleMod.py
+++ simpleMod.py
@@ -1,7 +1,5 @@
-from bs4 import BeautifulSoup as bs
 import os,time,sys,logging
-from shutil import rmtree
-import subprocess
+import random
 
 ##Check consistency of paramter
 ##generate the folders and files for the xp
@@ -33,18 +31,8 @@ class Experiment:
 
     #check if the score exist and return it, fi not return -1
     def gatherScore(self):
-        filename_score=os.path.join(self.particleDirectory,"score.txt")
-        try:
-            with open(filename_score,"r") as file_score:
-		try:
-                 	self.score=float(file_score.readline().strip())
-		except :
-            		logging.warning("score in a bad format")
-		self.clean()
-	
-        except IOError:
-            logging.debug(str(self)+" still loading")
-            self.score=-1
+        return(random.random())
+
     #check if the score exist and return it, fi not return -1
     def getScore(self):
         return(self.score)
@@ -53,37 +41,17 @@ class Experiment:
         result = 'experiment: '+str(self.expId)+' of kind:'+self.kind
         return result
 
+    ####methods called by launcher
+
     #generate a string that countain the command that should be run on marenostrum
     def generateTask(self):
-	n_years=50
-        #print("run pandora")
-        bashCommand = 'cd '+self.particleDirectory + ' && ./province && ./analysis ' +' && cd - &&'
-        bashCommand += '/apps/R/3.4.0/bin/Rscript --vanilla computeScore.R ' + self.particleDirectory + ' ' + str(n_years)+'\n'
-        #bashCommand += 'rm -rf '+os.path.join(self.particleDirectory,"data") + ' '+os.path.join(self.particleDirectory,"logs")+ ' '+os.path.join(self.particleDirectory,"*.gdf \n")
-        return bashCommand
         
     #remove the entire folder of the particul
     def remove(self):
-        try:
-            subprocess.Popen(["rm","-rf",self.particleDirectory])
-            logging.info("rm:"+self.expId+",score was:"+str(self.score))
-        except Exception as e:
-            print(e)
 
     #clean useless folder 
     def clean(self):
-        try:
-            subprocess.Popen(["rm","-rf",os.path.join(self.particleDirectory,"logs")])
-            subprocess.Popen(["rm","-rf",os.path.join(self.particleDirectory,"data")])
-            logging.debug("cleaned "+self.expId+" logs and data")
-        except Exception as e:
-            print(e)
 
     #move the particule forlder
     def softRemove(self):
-        try:
-            rmtree(self.particleDirectory)
-            logging.info("soft rm:"+self.expId+",score was:"+str(self.score))
-        except Exception as e:
-            print(e)
 
