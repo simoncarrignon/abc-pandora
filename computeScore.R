d7841bcc5f6af2d8c12b9b26249738dab5e7f9f4
Simon <simon@elisya.org>
2018-02-28 11:10:37 +0100

    Correction of two critical error in the score: runif that was sample TWICE similar site and count of the minimal that was not well done. Both problems were generating different results for two run that should be exactly the same.

--- computeScore.R
+++ computeScore.R
@@ -12,6 +12,11 @@ computeSimpsonForOneExpe  <-  function(expe,jf=sum,breaks,min)apply(agentWith(ex
 
 ##this function return the number of agent with at least on goods of the goods in the list "goods" and for the timestep in "timestep"
 #joinfucntion is the function used to group years put together, it also allow to takes only a subsample of the agents
+#goods = the goods that will be counted
+#timestep = the timestep used ie the interval between wich the sum (or ainy other `joinfunction`) will be counted
+#breaks = the timestep used 
+#numsite = if we are NOT using all the sites, one can: determine a number of site or give vector of number of site that will be used for each timestep
+#bias, if using a number of site < of the total number of agents then this allow to fix a percentage of the bigger sites that will be allways used
 agentWith <- function(expe,goods=NULL,timestep=NULL,breaks=NULL,joinfunction=sum,min=1,numsite=NULL,bias=NULL){
 	print(paste(numsite,bias,breaks))
 	
@@ -34,21 +39,21 @@ agentWith <- function(expe,goods=NULL,timestep=NULL,breaks=NULL,joinfunction=sum
 	else
 		names(numsite)=timestep
 	expe[is.na(expe)]=0
-	cur=expe[expe$p_good == "coins",]
+	cur=expe[expe$p_good == "coins",] #keep only the consumers (ie people producing "coins")
 	sapply( timestep, function(tmstp){
 	       cur=cur[cur$timeStep == tmstp,] 
 	       if(!is.null(bias)){
 		       tenpercent=(bias*numsite[tmstp])
 		       ranks=cur[cur$timeStep == unique(cur$timeStep)[1],c("agent","size")] #the rank change through time because i implemanted it like that thus I have to recompute the ranks each time. That sucks, and maybe I should force the size of the new consumer to always be low, and here don't have to worry anymore
 		       topten=ranks$agent[order(ranks$size,decreasing = T)][1:tenpercent] #when biased toward the big cities we sample the cite but by take at least the 10% of the biggest cities
-		       rest=cur$agent[!(cur$agent %in% topten )]
-		       random=unique(rest)[round(runif(numsite[tmstp]-tenpercent,1,length(unique(rest))))] 
+		       rest=cur$agent[!(cur$agent %in% topten )] #the agents from which we will randomly select
+		       random=unique(rest)[sample.int(length(unique(rest)))] 
 		       selectedAG=unlist(list(topten,random))
 
 	       }
 	       else{
 		       ##sampling is random
-		       selectedAG=levels(cur$agent)[round(runif(numsite[as.character(tmstp)],1,length(levels(cur$agent))))] 
+			selectedAG=levels(cur$agent)[sample.int(length(levels(cur$agent)))]
 	       }
 	       cur=cur[cur$agent %in% selectedAG,]
 	       cur=droplevels(cur)
@@ -56,12 +61,12 @@ agentWith <- function(expe,goods=NULL,timestep=NULL,breaks=NULL,joinfunction=sum
 
 		      if(!is.null(breaks)){#if we want to breaks the dataset in period then we need to put join the timestep of a same period using joinfunction (usually 'sum') 
 			      join=tapply(cur[,paste(g,"_q",sep="")],cur$agent,joinfunction)
-			      if(min==0)return(length(join[join>=min]))
-			      if(min==1)return(length(join[join>min]))
+			      if(min==0)return(length(join[join>min]))
+			      if(min==1)return(length(join[join>=min]))
 		      }
-		      else{ #if not we just count the number of agent with a quatnity > the min
-			      if(min==1)return(length(cur[ cur[,paste(g,"_q",sep="")] >= min,"agent"]))
+		      else{ #if not we just count the number of agent with a quantity > the min
 			      if(min==0)return(length(cur[ cur[,paste(g,"_q",sep="")] > min,"agent"]))
+			      if(min==1)return(length(cur[ cur[,paste(g,"_q",sep="")] >= min,"agent"]))
 		      }
 })
 
