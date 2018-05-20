1a913d1d269c75ecf5c7847464b6578991d841f0
Simon <simon@elisya.org>
2018-05-20 16:37:05 +0200

    remove a too depend part iof getlist of particle

--- abctools.R
+++ abctools.R
@@ -371,16 +371,13 @@ plotDensitiesFrompath <- function(path,param,epsilon,from,to,...){
     return(listParticles)
 }
 
-
 #take a path as argument and given that this path has a list of files of the form result_epsilon.csv
 #return a list with epsilon as names and the correspondig thetas => score dataframe
-
 getlistParticlesFromPath <- function(path){
 	lf=list.files(path,pattern="resul_*")
 	epsilon=sort(sub("result_(.*).csv","\\1",lf),decreasing=T)
 	listParticles=lapply(epsilon,function(eps){print(eps);cbind(read.csv(paste(path,"result_",eps,".csv",sep="") ),epsilon=eps)})
 	names(listParticles)=epsilon
-	listParticles=lapply(listParticles,function(u) {u$ee_peryear = u$nstep/500;u$CulturalEvents = u$nstep/u$cstep; u$ce_peryear = u$CulturalEvents/500;return(u)})#u$ratio = (u$cstep)/u$nstep; u$ee_perce = (u$nstep)/u$CulturalEvents;
 	return(listParticles)
 }
 
